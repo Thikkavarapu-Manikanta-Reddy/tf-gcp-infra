1ba043a3c655733151ab9b5d3784d7c055bfa85c
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-03-23 03:57:54 -0400

    Ops agent DNS Logging

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "google_compute_instance" "custom_vm_instance" {
   zone         = var.vm_instance_zone
   machine_type = var.vm_instance_machine_type
   tags         = var.vm_tag
-  depends_on   = [google_sql_database_instance.main_primary]
+  depends_on   = [google_service_account.vm_service_account, google_sql_database_instance.main_primary]
   boot_disk {
     initialize_params {
       image = var.vm_instance_image
@@ -89,6 +89,39 @@ resource "google_compute_instance" "custom_vm_instance" {
     DB_NAME                = var.google_sql_database_webapp_name,
     location               = var.properties_location
   })
+  service_account {
+    email  = google_service_account.vm_service_account.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_service_account" "vm_service_account" {
+  account_id   = "vm-instance-service-account"
+  display_name = "VM Service Account"
+  # create_ignore_already_exists = true
+}
+
+resource "google_project_iam_binding" "logging_roles_binding" {
+  project = var.projectId
+
+  role = "roles/logging.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.vm_service_account.email}",
+  ]
+  depends_on = [google_service_account.vm_service_account]
+}
+
+resource "google_project_iam_binding" "metric_roles_binding" {
+  project = var.projectId
+
+  role = "roles/monitoring.metricWriter"
+
+  members = [
+    "serviceAccount:${google_service_account.vm_service_account.email}",
+  ]
+
+  depends_on = [google_service_account.vm_service_account]
 }
 
 # Create a private address range for our network
@@ -143,4 +176,20 @@ resource "google_sql_user" "db_user" {
   name     = var.google_sql_user_webapp_name
   instance = google_sql_database_instance.main_primary.name
   password = random_password.password.result
+}
+
+data "google_dns_managed_zone" "env_dns_zone" {
+  name = "manikanta-reddy-thikkavarapu"
+}
+
+resource "google_dns_record_set" "a_dns_record" {
+  name         = data.google_dns_managed_zone.env_dns_zone.dns_name
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+  type         = "A"
+  ttl          = 60
+  rrdatas = [
+    google_compute_instance.custom_vm_instance.network_interface[0].access_config[0].nat_ip
+  ]
+
+  depends_on = [google_compute_instance.custom_vm_instance]
 }
